06dab20506d5708ebf011aa7bad49411b0549a46
daniloromano5932 <110908582+daniloromano5932@users.noreply.github.com>
2022-10-03 21:51:59 +0200

    Form HTML Creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import ToDoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="App">
-      
+      <ToDoForm />
     </div>
   );
 }
