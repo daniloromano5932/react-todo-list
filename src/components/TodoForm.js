ed39604bb6e8f9f86d1c8275f4450d92470eb7c6
daniloromano5932 <110908582+daniloromano5932@users.noreply.github.com>
2022-10-03 22:40:22 +0200

    Logic to the form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function ToDoForm() {
+
+  const [noteTitle, setNoteTitle] = useState("")
+  const [noteContent, setNoteContent] = useState("")
+
+  function saveNoteTitle(e) {
+    const newTitle = e.target.value;
+    setNoteTitle(newTitle);
+  }
+
+  function saveNoteContent(e) {
+    const newContent = e.target.value;
+    setNoteContent(newContent)
+  }
+
+  function addNote() {
+    console.log(noteTitle);
+    setNoteTitle("");
+  }
+
   return <form>
     <div>
-      <label for="title">Note Title</label>
-      <input type="text" name="title"/> </div>
+      <label for="title" >Note Title</label>
+      <input type="text" name="title" onChange={saveNoteTitle} value={noteTitle}/> </div>
     <div>
       <label for="content">Note Content</label>
-      <textarea name="content" /></div>
-      <button type="button">Add Note</button>
+      <textarea name="content" onChange={saveNoteContent} value={noteContent} /></div>
+      <button type="button" onClick={addNote}>Add Note</button>
   </form>
 }
 
